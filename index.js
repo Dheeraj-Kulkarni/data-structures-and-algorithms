44fa8bb59fa8f25d02bd4fd4bb7c2723169b862c
Noormohammed Shikalgar <noor.shikalgar03@gmail.com>
2022-11-14 22:46:29 +0530

    fix: added new folder for organized code

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const selectionSort = require("./selection-sort");
+const selectionSort = require("./algorithms/selection-sort");
 const numbers = [10,5,8,2,3,4,9];
 
 console.log("selection sort", selectionSort(numbers));
