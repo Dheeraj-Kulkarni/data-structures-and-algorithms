55479c682cd806ad0df8ac9b67d4cec82b5e9027
Noormohammed Shikalgar <noor.shikalgar03@gmail.com>
2022-11-14 22:59:20 +0530

    fix: added some minor change

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const selectionSort = require("./algorithms/selection-sort");
-const numbers = [10,5,8,2,3,4,9];
+const numbers = [10,8,2,3,4,9];
 
 console.log("selection sort", selectionSort(numbers));
 
